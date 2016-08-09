2d82a7ebcbbe3c1e3effb2af10fcdc8bb6d0173f
aniruddha <aniruddha.kudalkar@gmail.com>
2016-08-09 17:48:31 +0530

    initial setup

--- interactivitycommunication/src/main/java/com/codekul/interactivitycommunication/MainActivity.java
+++ interactivitycommunication/src/main/java/com/codekul/interactivitycommunication/MainActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String KEY_MY_NAME = "myName";
+    private static final int REQ_NEXT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +26,42 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent =
                         new Intent(MainActivity.this,cls);
-                startActivity(intent);
+
+                Bundle bundle =
+                        new Bundle();
+                bundle.putString(KEY_MY_NAME,getMyName());
+                intent.putExtras(bundle);
+
+                //startActivity(intent);
+                startActivityForResult(intent,REQ_NEXT);
             }
         });
+    }
+
+    private String getMyName(){
+
+        return ((EditText)findViewById(R.id.edtName))
+                .getText()
+                .toString();
+    }
+
+    private void setMyName(String text){
+        ((EditText)findViewById(R.id.edtName)).setText(text);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQ_NEXT){
+
+            if(resultCode == RESULT_OK){
+
+                Bundle bundle = data.getExtras();
+                String res = bundle.getString(NextActivity.KEY_DATA_RESULT);
+
+                setMyName(res);
+            }
+        }
     }
 }
--- interactivitycommunication/src/main/java/com/codekul/interactivitycommunication/NextActivity.java
+++ interactivitycommunication/src/main/java/com/codekul/interactivitycommunication/NextActivity.java
@@ -1,13 +1,51 @@
 package com.codekul.interactivitycommunication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NextActivity extends AppCompatActivity {
+
+    public static final String KEY_DATA_RESULT = "result";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        Intent responsibleIntent = getIntent();
+        Bundle bundle = responsibleIntent.getExtras();
+        if(bundle != null)
+            setData(bundle.getString(MainActivity.KEY_MY_NAME));
+
+        final Button btnOkay = (Button) findViewById(R.id.btnOkay);
+        btnOkay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent =
+                        new Intent();
+                Bundle bundle =
+                        new Bundle();
+                bundle.putString(KEY_DATA_RESULT,getData());
+
+                intent.putExtras(bundle);
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+
+    private void setData(String text){
+        ((EditText)findViewById(R.id.edtData)).setText(text);
+    }
+
+    private String getData(){
+        return  ((EditText)findViewById(R.id.edtData))
+                .getText()
+                .toString();
     }
 }
